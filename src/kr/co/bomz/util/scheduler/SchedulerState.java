ecc7e354129369120b47e141d7eecf4ab02c209f
bom bom <bom@bomz.app>
2021-10-01 13:50:53 +0000

    ver 1.1

---
+++ src/kr/co/bomz/util/scheduler/SchedulerState.java
@@ -0,0 +1,15 @@
+package kr.co.bomz.util.scheduler;
+
+/**
+ * 스케줄러 상태
+ * @author Bomz
+ * @since 1.0
+ * @version 1.0
+ *
+ */
+enum SchedulerState {
+	/**		스케줄러 시작 중		*/
+	START,
+	/**		스케줄러 정지 중		*/
+	STOP
+}
