0b0f7c37b082ce078f0b8e3e1ab16a7904732ddb
bom bom <bom@bomz.app>
2021-10-01 13:51:20 +0000

    ver 1.1

---
+++ src/kr/co/bomz/util/scheduler/SchedulerWeek.java
@@ -0,0 +1,26 @@
+package kr.co.bomz.util.scheduler;
+
+/**
+ * 
+ * 스케줄러가 요일별로 동작할 경우 사용되는 요일 타입
+ * @author Bomz
+ * @since 1.0
+ * @version 1.0
+ *
+ */
+public enum SchedulerWeek {
+	/**		일요일		*/
+	SUNDAY,
+	/**		월요일		*/
+	MONDAY,
+	/**		화요일		*/
+	TUESDAY,
+	/**		수요일		*/
+	WEDNESDAY,
+	/**		목요일		*/
+	THURSDAY,
+	/**		금요일		*/
+	FRIDAY,
+	/**		토요일		*/
+	SATURDAY
+}
